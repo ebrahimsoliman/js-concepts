873ac83bdae4021c83106a35b9c3633e51c5219b
Ebrahim Ahmed <ebrahim.ahmed@nfq.com>
2024-03-29 01:10:16 +0200

    Add new JavaScript file "global.js"
    Introduced a new file, "global.js", to explain the concept of global objects in JavaScript. The file demonstrates the usage of global objects through examples and comments, aiming to assist in understanding the 'global' concept in JavaScript contexts for both server-side and browser-side scenarios.

---
+++ global.js
@@ -0,0 +1,17 @@
+//try this on browser console
+
+// global object
+// global in javascript means not inside a function or block
+
+
+//for server
+
+console.log(this);
+//for browser
+
+console.log(window);
+
+let a = 10;
+
+function b() { /* empty function */ }
+//you will find the variable a and function b in the global object
