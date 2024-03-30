fde44537df8f3c337193d74bbe69ae696188d7cd
Ebrahim Ahmed <ebrahim.ahmed@nfq.com>
2024-03-30 03:00:02 +0200

    Add file explaining JavaScript operators usage
    Implemented a new file "operators.js" under ./js-concepts/types which outlines and demonstrates the various usages (infix, prefix, and postfix) of operators in JavaScript. The file aids as an introductory guide for comprehending the functionalities and utilisation of operators with clear examples.

---
+++ types/operators.js
@@ -0,0 +1,39 @@
+// operators are special function that is syntactically written differently it takes two parameters and return one result
+
+// infix notation
+//these are the infix notation of the operators
+//infix notation is when the operator is in between the two operands
+
+console.log(1 + 2); //3
+console.log(2 - 1); //1
+console.log(2 * 2); //4
+console.log(4 / 2); //2
+console.log(4 % 2); //0
+console.log(2 ** 3); //8
+
+//prefix notation
+//these are the prefix notation of the operators
+//prefix notation is when the operator is before the two operands
+console.log(+1); //1
+console.log(-1); //-1
+console.log(!true); //false
+console.log(!!true); //true
+console.log(typeof 1); //number
+console.log(typeof 'hello'); //string
+let a = 1;
+console.log(++a); //2
+console.log(--a); //1
+// postfix notation
+//these are the postfix notation of the operators
+//postfix notation is when the operator is after the two operands
+let a = 1;
+console.log(a++); //1
+console.log(a); //2
+console.log(a--); //2
+console.log(a); //1
+console.log(a += 1); //2
+console.log(a); //2
+console.log(a -= 1); //1
+console.log(a); //1
+console.log(a *= 2); //2
+console.log(a); //2
