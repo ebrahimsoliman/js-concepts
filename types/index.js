c1b4ce3de8b7bf55ad2814ed26861be1f8b2b606
Ebrahim Ahmed <ebrahim.ahmed@nfq.com>
2024-03-30 02:43:40 +0200

    Add JavaScript primitive types concept file
    Introduced a new file called index.js in ./js-concepts/types. This file provides a thorough explanation and demonstration of the six primitive types in JavaScript: undefined, null, boolean, number, and string. This supports learning foundational JS knowledge about variable types, their assignment, and behavior.

---
+++ types/index.js
@@ -0,0 +1,26 @@
+//js is dynamically typed language
+// you don't tell the engine what type of data a variable holds
+// it figures it out while your code is running
+// variables can hold different types of values because it's all figured out during execution
+
+// Primitive Types
+// a type of data that represents a single value
+// that is not an object ,and it is immutable (cannot be changed)
+//there are six primitive types in js
+
+// 1-undefined represents lack of existence (you shouldn't set a variable to this)
+const a = undefined;
+
+// 2-null represents lack of existence (you can set a variable to this)
+const b = null;
+
+// 3-boolean true or false
+const c = true;
+
+// 4-number floating point number (there's always some decimal) unlike other programming languages
+// there is only one number type in js and it can make math weird
+const d = 1;
+
+// 5-string a sequence of characters both '' and "" can be used
+const e = 'hello';
+
